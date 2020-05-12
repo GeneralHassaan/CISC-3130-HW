ea421560c893bac87629c2f27cd0f66fc184e1fa
GeneralHassaan <56697897+GeneralHassaan@users.noreply.github.com>
2020-05-12 05:18:39 -0400

    Add files via upload
    assignment 1

---
+++ Artist.java
@@ -0,0 +1,52 @@
+//HASSAAN ASIF
+//TANVIR AHMED
+// CISC 3130 MY9
+
+public class Artist {
+
+	// Attributes..
+	private String name;
+	private Artist next;
+	
+	// Constructor..
+	public Artist(String name) {
+		
+		this.name = name;
+		this.next = null;
+	
+	}
+	
+	public Artist(String name, Artist next) {
+	
+		this.name = name;
+		this.next = next;
+	
+	}
+
+	// Getter and Setters..
+	
+	public String getName() {
+
+		return name;
+	
+	}
+
+	public void setName(String name) {
+	
+		this.name = name;
+	
+	}
+
+	public Artist getNext() {
+	
+		return next;
+	
+	}
+
+	public void setNext(Artist next) {
+	
+		this.next = next;
+	
+	}
+	
+}
